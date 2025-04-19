c1c1c397f4aa08a3a0b709da1512f2954db351fe
Elena <ed41658@gmail.com>
2025-04-19 14:11:15 +0500

    create Button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import styles from "./button.module.scss";
+
+interface IButtonProps {
+  children: React.ReactNode,
+  use: string,
+  handler: () => void,
+  disabled?: boolean
+}
+
+const Button: React.FC<IButtonProps> = ({ children, use, handler, disabled }) => {
+  return (
+    <button
+      className={`${styles.btn} ${styles[`btn--${use}`]}`}
+      onClick={handler}
+      disabled={disabled}
+    >
+    {children}
+    </button>
+  );
+};
+
+export default Button;
--- src/components/NicknameForm/NicknameForm.tsx
+++ src/components/NicknameForm/NicknameForm.tsx
@@ -2,6 +2,7 @@ import { IoMdClose } from "react-icons/io";
 import styles from "./nicknameForm.module.scss";
 import cn from 'classnames';
 import Image from "../Image/Image";
+import Button from "../Button/Button";
 
 const NicknameForm = () => {
   return (
@@ -22,8 +23,8 @@ const NicknameForm = () => {
                 <label className={cn(styles["input-group__title"])} htmlFor="name">Ваше имя</label>
                 <input className={cn(styles["input-group__write"])} type="text" id="name" placeholder="Введите ваше имя"></input>
             </div>
-
-            <button className={cn(styles.btn, styles["btn-generate"])} >Сгенерировать ник</button>
+            
+            <Button use="generate" handler={()=>{}}>Сгенерировать ник</Button>
             
             <div className={cn(styles["nickname-form__container"])}>
                 <h3 className={cn(styles["nickname-form__result-title"])}>Ваш новый ник:</h3>
